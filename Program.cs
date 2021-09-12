a92b435cd397bd0e85a83e0f83dca6f7300f715e
Abdul-lateef Olarenwaju Adeleye <Adeleye@lotusbetaanalytics.com>
2021-09-12 22:11:46 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace InversePermutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1,4,3,2 };
+            int size = arr.Length;
+            inversePermutation(arr, size);
+        }
+
+        static void inversePermutation(int[] arr, int size)
+        {
+           
+            // Loop to select Elements one by one
+            for (int i = 0; i < size; i++)
+            {
+
+                // Loop to print position of element
+                // where we find an element
+                for (int j = 0; j < size; j++)
+                {
+
+                    // checking the element in
+                    // increasing order
+                    if (arr[j] == i + 1)
+                    {
+                       
+                        Console.Write(j + 1 + " ");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
